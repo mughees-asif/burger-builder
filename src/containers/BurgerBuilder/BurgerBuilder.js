6fe063c1fe6c525ccfa3f747738c40c559dbb546
mughees-asif <mughees460@gmail.com>
2020-04-20 21:09:43 +0100

    state change

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,9 +3,18 @@ import Aux from '../../hoc/AuxL';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+	state = {
+		ingredients: {
+			salad: 1,
+			bacon: 1,
+			cheese: 2,
+			meat: 2
+		}
+	}
 	render() {
 		return (
-			<Aux>
+			<Aux ingredients={ this.state.ingredients }>
 				<Burger />
 			</Aux>
 		);
